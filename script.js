c2d03f0b1b618372174e6754b0b75cc7ebc989c1
Genone22 <100020872+Genone22@users.noreply.github.com>
2023-07-06 10:37:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 function eduregion_sc(cl, pu) {
     let d = new Date(); // Создание нового объекта Date для работы с датой и временем.
     d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000)); // Установка времени истечения срока действия куки на 1 год.
-    document.cookie = "cl_uid=" + cl + ";" + "expires=" + d.toUTCString() + ";path=/; domain=eduregion.ru"; // Установка куки "cl_uid" с заданным значением и сроком действия на домене "eduregion.ru".
+    document.cookie = "cl_uid=" + cl + ";" + "expires=" + d.toUTCString() + ";path=/; domain=domen.ru"; // Установка куки "cl_uid" с заданным значением и сроком действия на домене "eduregion.ru".
 
-    document.cookie = "publ_uid=" + pu + ";" + "expires=" + d.toUTCString() + ";path=/; domain=eduregion.ru"; // Установка куки "publ_uid" с заданным значением и сроком действия на домене "eduregion.ru".
+    document.cookie = "publ_uid=" + pu + ";" + "expires=" + d.toUTCString() + ";path=/; domain=domen.ru"; // Установка куки "publ_uid" с заданным значением и сроком действия на домене "eduregion.ru".
 
     localStorage.setItem('cl_uid', cl); // Сохранение значения переменной "cl" в локальном хранилище.
     localStorage.setItem('publ_uid', pu); // Сохранение значения переменной "pu" в локальном хранилище.
